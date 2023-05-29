45294b68161ccca1512d2f68f69a6b3c83edea5c
zerobug110 <mahabojang110@gmail.com>
2023-05-29 19:08:19 +0000

    sent the data to tthe json file

--- server/index.js
+++ server/index.js
@@ -5,8 +5,9 @@ const db = require("./models")
 const port = 3001
 //routes 
 const postRouter = require("./routes/post")
+
 //middle wares
-app.use("/post", postRouter)
+app.use("/posts", postRouter)
 app.use(express.json());
 
 
