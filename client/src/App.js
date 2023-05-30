f00d575ec15506ffd1377a887b7f74b5476f18ab
zigtech <zerobug110>
2023-05-30 13:12:57 +0000

    connect the server with the client and made api call

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useEffect, useState} from 'react'
+import axios from 'axios';
 
 function App() {
+  const [postList, setPostList] = useState([])
+  const baseUrl = "http://localhost:3001/posts"
+
+  useEffect(()=> {
+    axios.get(`${baseUrl}`).then((response) => {
+      setPostList(response.data);      
+    })
+  }, [])
+  // useEffect
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        postList.map( (item) => {
+          return(
+            <h1>{item.title}</h1>
+          )
+        })
+      }
     </div>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require("express")
 const app = express();
 const db = require("./models")
+const cors = require("cors")
 
 const port = 3001
 //routes 
 const postRouter = require("./routes/post")
 
-app.use(express.json());
 //middle wares
+app.use(express.json());
+app.use(cors())
 app.use("/posts", postRouter)
+
 
 
 db.sequelize.sync().then(()=> {
