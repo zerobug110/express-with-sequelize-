ea6dc01ecba54b22281daf29b352c01ab9e71aae
zigtech <mahabojang110@gmail.com>
2023-06-02 12:22:54 +0000

    added add comment

--- client/src/pages/post/post.js
+++ client/src/pages/post/post.js
@@ -2,8 +2,12 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
 import "./post.css";
+
+
 export const Post = () => {
   const [post, setPost] = useState({})
+  const [comments, setComments] = useState([])
+  const [newComment, setNewComment] = useState([])
   let {slug} = useParams()
 
   useEffect(()=>{
@@ -11,6 +15,22 @@ export const Post = () => {
       setPost(response.data)
     })
   },[])
+
+  useEffect(()=>{
+    axios.get(`http://localhost:3001/comments/${slug}`).then((response)=> {
+      setComments(response.data)
+    })
+  },[])
+
+  const addComment = () => {
+    axios.post("http://localhost:3001/comments", 
+    {commentBody: newComment, PostId: slug}).
+    then((response)=> {
+      const commentsToAdd = {commentBody: newComment}
+      setComments([...comments, commentsToAdd])
+    })
+  }
+
   return(
     <div className="post">
       <div className="blog-card">
@@ -21,7 +41,18 @@ export const Post = () => {
         <div className="blog-text">{post.postText}</div>
         <div className="blog-footer">{post.username}</div>
       </div>
-      <div className="comment">comment</div>
+
+        <div className="comment">
+          <input placeholder="comment-list" onChange={(event)=> {setNewComment(event.target.value)}} value={newComment}/>
+          <button onClick={addComment}> add comment</button>
+          {
+            comments.map( comment => {
+              return(
+                <div className="comment-message">{comment.commentBody}</div>
+              )
+            })
+          }
+        </div>
     </div>
     
   )
